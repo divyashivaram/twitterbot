5ff82ae4f78820218ca2fc61a78ed5aee55418d7
divya <div.shivaram@gmail.com>
2014-09-04 01:57:18 +0530

    added auto RT

--- twitterbot.py
+++ twitterbot.py
@@ -8,4 +8,17 @@ app_secret = "JdjdYqPgd9SYvxZ0egrc3jFHGr8gvHFpDwXwVzzFJbsQlqPd5C"
 TwitterHandle = "turbotwitbot"
 
 twitter = Twython(app_key, app_secret, Oauth_token, Oauth_secret)
-twitter.update_status(status = "Hello World")
+def update_status(s):
+    twitter.update_status(status = s)
+
+def retweet(q, count=10):
+    # twitter.retweet(id = "507254244173307904")
+    search_results = twitter.search(q=q, count=count)
+    try:
+        for tweet in search_results["statuses"]:
+            twitter.retweet(id = tweet["id_str"])
+    except TwythonError as e:
+            print e
+
+# retweet(q = "Robots", count = 10)
+
